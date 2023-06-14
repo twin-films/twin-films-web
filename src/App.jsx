62719e8879a5e6f3a44723c3de67509c075432da
Ignat Miagkov <iamiagkov@email.wm.edu>
2023-06-13 23:25:37 -0400

    Some dependency updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Website by Ignat and Emanuel!
       </p>
     </>
   )
